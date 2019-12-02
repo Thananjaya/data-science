644ae7e7cfe585aeabd56eca68fd92d5c79aee6e
Thananjaya <thananjaya091993@gmail.com>
2019-12-02 20:40:15 +0530

    calculating the percentage using assign function

--- bitcoin_market_analysis.py
+++ bitcoin_market_analysis.py
@@ -10,3 +10,12 @@ print(bt.info(), 'General information of the data frame\n')
 just_usd_market = bt[['id', 'market_cap_usd']]
 cap = just_usd_market[just_usd_market.market_cap_usd > 0]
 print(cap.count(), 'Checking the total observations of the market cap usd.')
+
+#calculate the cercentage in US market using assign function
+total_sum = cap.market_cap_usd.sum()
+cap10 = cap.head(10).set_index('id')
+cap10 = cap10.assign(market_cap_perc= lambda x : (x.market_cap_usd/total_sum)*100)
+ax = cap10.plot.bar(title='Top 10 market capitalization')
+ax.set_ylabel('% of total cap')
+plt.xlabel('USD')
+plt.show()
