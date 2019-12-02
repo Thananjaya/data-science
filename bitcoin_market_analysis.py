82d3d4b4336d802e72dfa8622b448895e2e6d5f8
Thananjaya <thananjaya091993@gmail.com>
2019-12-02 22:58:39 +0530

    added the csv and bar graph been plotted for different market cap

--- bitcoin_market_analysis.py
+++ bitcoin_market_analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # reading the json file as dataframe using read_json
-bt = pd.read_json('https://api.coinmarketcap.com/v1/ticker/')
+bt = pd.read_csv('./csv/bitcoin_2017.csv')
 print(bt.info(), 'General information of the data frame\n')
 
 # We dont need the coins which  dont have US market
@@ -29,7 +29,7 @@ print(volatility.head(), '\nChecking the market changes per 24h time period\n')
 print('it looks like the changes are huge\n')
 
 
-#ploting the top 10 winners and tht top 10 loosers in the US market as per 24h change
+
 def top10_winners_loosers(volatility_series, title):
   # Making the subplot and the figure for two side by side plots, where 10 represents width and 6 represents height
   fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 6))
@@ -41,4 +41,26 @@ def top10_winners_loosers(volatility_series, title):
   ax = volatility_series[-10:].plot.bar(ax=axes[1], color='red')
   plt.show()
 
+#ploting the top 10 winners and tht top 10 loosers in the US market as per 24h change
 top10_winners_loosers(volatility['percent_change_24h'].sort_values().dropna(), '24 hours top losers and winners')
+
+#ploting the top 10 winners and tht top 10 loosers in the US market change weekly
+top10_winners_loosers(volatility.sort_values('percent_change_7d').dropna()['percent_change_7d'], 'Per Week top losers and winners')
+
+# filtering the coin which has more than 1 billion market cap
+largecaps = cap[cap['market_cap_usd'] > 10000000000]
+print(largecaps)
+
+#filtering the coins with respect to the market cap and plotting them
+def capcount(query_string):
+  return cap.query(query_string).count().id
+
+biggish = capcount('market_cap_usd > 300000000')
+micro = capcount('market_cap_usd > 50000000 & market_cap_usd < 300000000')
+nano =  capcount('market_cap_usd < 50000000')
+bar_index = list(range(3))
+labels = ["biggish", "micro", "nano"]
+values = [biggish, micro, nano]
+plt.bar(bar_index, values)
+plt.xticks(bar_index, labels ,rotation=30)
+plt.show()
