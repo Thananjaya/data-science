38daeacd0467ea1f03036945fbfbc04f5bb7d455
Thananjaya <thananjaya091993@gmail.com>
2020-01-03 11:41:12 +0530

    analysing the lego datasets done

--- lego-analysis.py
+++ lego-analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #analysing the lego colors datasets
 lego_colors = pd.read_csv('csv/lego_colors.csv')
@@ -18,3 +19,20 @@ print(trans_nontrans, 'displaying the total count of transparent coors and non-t
 lego_sets = pd.read_csv('csv/lego_sets.csv')
 print(lego_sets.info(), 'analysing the coloumn names and its dtypes using info() method\n')
 print(lego_sets.head(5), 'printing the first 5 rows of the data sets\n')
+
+#checking for the number of themes sold per year
+num_theme = lego_sets[['year', 'theme_id']].groupby('year').count()
+print(num_theme.head(), 'theme id vs grouped year\n')
+plt.plot(num_theme)
+plt.xlabel('years -->')
+plt.ylabel('Theme id -->')
+plt.show()
+
+#checking for the average lego parts per year
+parts_by_year = lego_sets[['year', 'num_parts']].groupby('year', as_index=False).mean().round(2)
+print(parts_by_year.head(6), 'parts by year vs grouped year')
+plt.plot(parts_by_year, color='r')
+plt.xlabel('Year')
+plt.ylabel('Average Number of Parts')
+plt.title('Average Lego Parts by Year')
+plt.show()
