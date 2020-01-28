eda42aabab6aeadb0d3b3ac379587e89d934dedd
Thananjaya <thananjaya091993@gmail.com>
2020-01-28 11:41:43 +0530

    fitting the data is on progress

--- credit_card_approvals.py
+++ credit_card_approvals.py
@@ -1,16 +1,20 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.preprocessing import LabelEncoder, scale
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
 #reading the datasets from the csv file using pandas dataframe
 df = pd.read_csv('./csv/cc_approvals.data', header=None)
-df.columns = ['gender', 'age', 'debt', 'married', 'bank_customer', 'education', 'ethnicity', 'years_employed', 'prior_default', 'employed', 'credit_score', 
-	'drivers_license', 'citizen', 'zip_code', 'income', 'approved']
 
 # inspecting the dataframe ad checking the dtypes, also other aspects in the dataframe
 print('\ndisplaying the first 20 records\n', df.head(20))
 print(df.info(), 'checking the dtypes of the dataframe\n')
 print('\ndisplaying the summary statistics of all the numerical rows\n', df.describe())
+
+df.columns = ['gender', 'age', 'debt', 'married', 'bank_customer', 'education', 'ethnicity', 'years_employed', 'prior_default', 'employed', 'credit_score', 
+	'drivers_license', 'citizen', 'zip_code', 'income', 'approved']
 
 #checking for missing values in the dataframe
 print('\nchecking for the missing values\n',df.tail(20))
@@ -18,7 +22,9 @@ df = df.replace('?', np.NaN) #replacing the ? symbol with the Nan
 print('\nchecking the dataframe after replacing the missing values with NaN\n',df.tail(20))
 
 #replacing the missing values column wise using ffill, which will fill the missing value with the closer value
-df = df.fillna(method='ffill')
+for col in df.columns:
+	if df[col].dtypes == 'object':
+		df[col] = df[col].fillna(method='ffill')
 print('\nchecking the dataframe after replacing the missing values with the method ffill\n',df.tail(20))
 
 #datasets consists of objects which cane be of categorical type. We have to convert those in to integers
@@ -30,14 +36,22 @@ for col in df.columns:
 	if df[col].dtypes == 'object':
 		df[col] = le.fit_transform(df[col])
 
-print('\nreplacing the object type in to integer type\n', df.head())
+print('\nreplacing the object type in to integer type using label encoder\n', df.head())
 
-#if certain values in the features is high, it would have much impact on deciding the credit approval
-#so we scale the values column wise between the ranges min=0 and max=1
-X = df.iloc[:, :13]
+#seperating the feature variables and target variable
+X = df.iloc[:, 0:13]
 y = df.iloc[:, 13]
-scaler = MinMaxScaler(feature_range=(0,1))
-scaledX = scaler.fit_transform(X)
-print(scaledX)
 
+# #if certain values in the features is high, it would have much impact on deciding the credit approval
+# # x- mean / variance, we use this formula to bind the values closer
+# for col in X.columns:
+# 	X[col] = scale(df[col])
+
+#splitting the datasets in to train data and test data
+X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+print(X_train.shape, y_train.shape)
+
+#using logistic regression, since credit card approval is of classification problem 
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
 
