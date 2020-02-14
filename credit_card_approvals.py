4aa4f411c5db795cbf13a5c80e1f363a7dbc6e60
thananjaya <thananjaya091993@gmail.com>
2020-02-14 13:16:55 +0530

    modification in the data  preprocessing

--- credit_card_approvals.py
+++ credit_card_approvals.py
@@ -2,30 +2,27 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, scale
 from sklearn.model_selection import train_test_split, GridSearchCV
+# from sklearn.ensemble import VotingClassifier, RandomForestClassifier>
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 
 #reading the datasets from the csv file using pandas dataframe
 df = pd.read_csv('./csv/cc_approvals.data', header=None)
+df.columns = ['gender', 'age', 'debt', 'married', 'bank_customer', 'education', 'ethnicity', 'years_employed', 'prior_default', 'employed', 'credit_score', 
+	'drivers_license', 'citizen', 'zip_code', 'income', 'approved']
 
 # inspecting the dataframe ad checking the dtypes, also other aspects in the dataframe
 print('\ndisplaying the first 20 records\n', df.head(20))
 print(df.info(), 'checking the dtypes of the dataframe\n')
 print('\ndisplaying the summary statistics of all the numerical rows\n', df.describe())
 
-df.columns = ['gender', 'age', 'debt', 'married', 'bank_customer', 'education', 'ethnicity', 'years_employed', 'prior_default', 'employed', 'credit_score', 
-	'drivers_license', 'citizen', 'zip_code', 'income', 'approved']
-
 #checking for missing values in the dataframe
 print('\nchecking for the missing values\n',df.tail(20))
 df = df.replace('?', np.NaN) #replacing the ? symbol with the Nan
 print('\nchecking the dataframe after replacing the missing values with NaN\n',df.tail(20))
 
-#replacing the missing values column wise using ffill, which will fill the missing value with the closer value
-for col in df.columns:
-	if df[col].dtypes == 'object':
-		df[col] = df[col].fillna(method='ffill')
-print('\nchecking the dataframe after replacing the missing values with the method ffill\n',df.tail(20))
+df.age = df.age.astype(float)
+df = df.dropna(axis=0)
 
 #datasets consists of objects which cane be of categorical type. We have to convert those in to integers
 #we will be using label encoder to convert the categorical object type in to integer type
@@ -35,14 +32,16 @@ le = LabelEncoder()
 for col in df.columns:
 	if df[col].dtypes == 'object':
 		df[col] = le.fit_transform(df[col])
+	elif df[col].dtypes == 'float64':
+		df[col] = scale(df[col])
 
 print('\nreplacing the object type in to integer type using label encoder\n', df.head())
 
-#seperating the feature variables and target variable
-X = scale(df.drop('approved', axis=1).values)
+# seperating the feature variables and target variable
+X = df.drop('approved', axis=1).values
 y = df.approved.values
 
-#splitting the datasets in to train data and test data
+# splitting the datasets in to train data and test data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 #using logistic regression, since credit card approval is of classification problem
@@ -55,7 +54,7 @@ y_predict = logreg.predict(X_test)
 
 #model performance calculation
 print("Accuracy of logistic regression classifier: ", logreg.score(X_test, y_test))
-print('\nConfusion matrix\n', confusion_matrix(y_predict, y_test))
+print('\nConfusion matrix\n', confusion_matrix(y_test, y_predict))
 
 #hyperparameters tuning
 #Define the grid of values for tol and max_iter
@@ -67,7 +66,7 @@ param_grid = {'tol': tol, 'max_iter': max_iter}
 
 # Instantiate GridSearchCV with the required parameters
 grid_model = GridSearchCV(estimator=logreg, param_grid=param_grid, cv=5)
-cv_results = grid_model.fit(X, y)
+cv_results = grid_model.fit(X_train, y_train)
 
 best_params, best_score = cv_results.best_params_, cv_results.best_score_
 print("The best params are", best_params)
